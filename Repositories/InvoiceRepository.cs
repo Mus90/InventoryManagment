4a1435f33a49e78a53b01e9f5b0061a173e2dc87
mustafa <mustafaalamin.07@gmail.com>
2021-10-27 19:08:08 +0200

    add repository abstraction layer

---
+++ Repositories/InvoiceRepository.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagement.Data;
+using InventoryManagement.Models;
+using InventoryManagement.Models.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Repositories
+{
+    public class InvoiceRepository : RepositoryBase<Invoice>
+    {
+        private readonly ApplicationDbContext _context;
+        public InvoiceRepository(ApplicationDbContext appContext) :
+            base(appContext)
+        {
+            _context = appContext;
+        }
+
+
+    }
+}
